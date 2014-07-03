8ce33c098ad91e9578386aaa5b683f1efac0da61
Alessio Zappa <alessio@plasticpanda.com>
2014-07-03 17:31:24 +0200

    update meta tags

--- bin/livecompile.sh
+++ bin/livecompile.sh
@@ -1,4 +1,4 @@
 # call me from parent dir!
 
-static -H '{"Cache-Control": "no-cache, must-revalidate"}' --port 3000 &
+static -H '{"Cache-Control": "no-cache, must-revalidate"}' --port 3010 &
 nodemon --exec "sh $(pwd)/bin/build.sh" -e jade,styl,js --ignore "$(pwd)/static/js/main.min.js"
