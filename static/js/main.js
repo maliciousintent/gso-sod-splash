3a40d1a9d4fb77bf6b6ca85c627f60533058291e
Alessio Zappa <alessio@plasticpanda.com>
2014-07-17 13:28:47 +0200

    update with simple countdown

--- static/js/main.js
+++ static/js/main.js
@@ -4,8 +4,9 @@
 
 $(function () {
   'use strict';
+  var _timer;
 
-
+  /*
   //  -- clock --
   $('.clock-countdown').downCount({
     date: '11/20/2014 11:00:00',
@@ -15,6 +16,23 @@ $(function () {
     $('.c-hour, .c-day', $('.clock-container')).remove();
     $('.clock-container').append('<div class="clock-zero"><h4 class="zeroday-txt">' + zerodayText + '</h4></div>');
   });
+  */
+
+  //  -- simple countdown --
+
+  function getDifferenceTime () {
+
+    var _now = new Date().getTime();
+    var _theDate = new Date('November 20, 2014 9:30 am').getTime();
+
+    console.log(humanizeDuration((_theDate - _now), 'it', 'array'));
+
+  }
+
+  _timer = setInterval(function () {
+    getDifferenceTime();
+  }, 1000);
+
 
   $('.fadeIn').addClass('show');
 
