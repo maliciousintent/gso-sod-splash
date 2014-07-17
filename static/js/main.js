d164f1f81ba30a2bfa62424d9db68ee7d7bcc4f0
Alessio Zappa <alessio@plasticpanda.com>
2014-07-17 17:23:43 +0200

    added speakers slider, remove countdown, added menu and agenda

--- static/js/main.js
+++ static/js/main.js
@@ -1,39 +1,71 @@
 /*jshint browser:true, laxcomma:true, eqnull:true, indent:2, unused:true, undef:true, devel:true*/
-/*globals $*/
+/*globals $, Swipe*/
 
 
 $(function () {
   'use strict';
-  var _timer;
-
   /*
-  //  -- clock --
-  $('.clock-countdown').downCount({
-    date: '11/20/2014 11:00:00',
-    offset: +1
-  }, function () {
-    var zerodayText = 'Ci siamo!';  
-    $('.c-hour, .c-day', $('.clock-container')).remove();
-    $('.clock-container').append('<div class="clock-zero"><h4 class="zeroday-txt">' + zerodayText + '</h4></div>');
-  });
-  */
 
   //  -- simple countdown --
+  var _timer;
 
   function getDifferenceTime () {
+    //  604800000 ms = 1 week
+    //  86400000 ms = 1 day
 
     var _now = new Date().getTime();
     var _theDate = new Date('November 20, 2014 9:30 am').getTime();
+    var _diff = _theDate - _now;
+    var _countdownArray = humanizeDuration(_diff, 'it', 'array');
+    //  -- [] = mesi, giorni, ore, minuti, secondi --
 
-    console.log(humanizeDuration((_theDate - _now), 'it', 'array'));
-
+    //  more than 1 week
+    if (_diff > 604800000) {
+      $('#countdown > span').text(_countdownArray[0] + ' e ' + _countdownArray[1]);
+    } else if (_diff > 86400000) {
+    //  more than 1 day
+      $('#countdown > span').text(_countdownArray[1] + ' e ' + _countdownArray[2]);
+    } else {
+    //  almost there!
+      $('#countdown > span').text(_countdownArray[2] + ', ' + _countdownArray[3] + ' e ' + _countdownArray[4]);
+    }
   }
+  getDifferenceTime();
 
   _timer = setInterval(function () {
     getDifferenceTime();
   }, 1000);
+  */
 
 
+
+  //  -- on page load fade in elements --
   $('.fadeIn').addClass('show');
+
+
+  //  -- speakers slider --
+  window.speakersSlide = new Swipe(document.getElementById('speakers'), {
+    speed: 1000,
+    auto: 3000,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    transitionEnd: function(i, el) {
+      var _i = $(el).attr('data-slide-id');
+      $('#speakers-thumbs span.pic').removeClass('active');
+      $('#speakers-thumbs span.pic').eq(_i).addClass('active');
+    }
+  });
+
+  $('#speakers-thumbs span.pic > img').click(function (e) {
+    e.preventDefault();
+    var _i = $(this).attr('data-speaker-id');
+    window.speakersSlide.slide(_i, 1000);
+    $('#speakers-thumbs span.pic').removeClass('active');
+    $('#speakers-thumbs span.pic').eq(_i).addClass('active');
+    return false;
+  });
+
+
 
 });
